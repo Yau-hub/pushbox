d5f705ea2cd3d747b94efe3c47df7539333b2fd2
Yau <1173383184@qq.com>
2021-08-04 00:18:56 +0800

    init bg

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -9,30 +9,81 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
+        oneSayLabel: {
+            default: null,
+            type: cc.Label
+        }
     },
+
+
+
 
     // LIFE-CYCLE CALLBACKS:
 
-    // onLoad () {},
+     onLoad () {
+         this.oneSay();
+     },
 
     start () {
+        let that = this;
 
+        this.loadImg();
+
+
+
+
+        setInterval(function () {
+            that.oneSay();
+        },10000)
     },
 
+
     // update (dt) {},
+
+    loadImg: function(){
+        var that = this;
+        let container = cc.find('Canvas/mainBg/bingBg');//图片呈现位置
+        var imgServeUrl = 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1';
+        var imgUrl = '';
+        let xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && (xhr.status >= 200 && xhr.status < 400)) {
+                var response =  JSON.parse(xhr.responseText);
+                imgUrl = 'https://cn.bing.com' + response.images[0].urlbase + '_720x1280.jpg'
+
+                cc.assetManager.loadRemote(imgUrl, function (err, texture) {
+                    var sprite  = new cc.SpriteFrame(texture);
+                    container.spriteFrame = sprite;
+                    //创建一个使用图片资源的新节点对象
+                    let starNode = new cc.Node(); //创建一个新节点
+                    starNode.name = "star1";
+                    starNode.setPosition(0,0);
+                    let starSprite = starNode.addComponent(cc.Sprite); //增加精灵组件
+                    starSprite.spriteFrame = sprite; //设置精灵组件图片资源
+                    starSprite.sizeMode = 'CUSTOM';
+                    starSprite.node.width = cc.winSize.width
+                    starSprite.node.height = cc.winSize.height
+                    container.addChild(starNode); //场景中增加新节点
+                });
+            }
+        };
+        xhr.open("get", imgServeUrl, true);
+        xhr.send();
+    },
+    oneSay(){
+        let that = this;
+        let url = "https://v1.hitokoto.cn/";
+        let xhr = new XMLHttpRequest();
+        let oneSayLabel = cc.find("Canvas/mainBg/oneSay").getComponent(cc.Component);
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && (xhr.status >= 200 && xhr.status < 400)) {
+                var response =  JSON.parse(xhr.responseText);
+               if(that.oneSayLabel && that.oneSayLabel.string != null) that.oneSayLabel.string = response.hitokoto + ' -- ' +  response.from;
+               else if(oneSayLabel && oneSayLabel.string != null ) oneSayLabel.string = response.hitokoto + ' -- ' +  response.from;
+            }
+        };
+        xhr.open("get", url, true);
+        xhr.send();
+    }
 });
--- library/imports/22/2276cfa3-0876-4119-85fe-271cd7c8a0c5.js
+++ library/imports/22/2276cfa3-0876-4119-85fe-271cd7c8a0c5.js
@@ -1,6 +1,6 @@
 "use strict";
 cc._RF.push(module, '2276c+jCHZBGYX+JxzXyKDF', 'main');
-// main.js
+// scripts/main.js
 
 "use strict";
 
@@ -12,26 +12,75 @@ cc._RF.push(module, '2276c+jCHZBGYX+JxzXyKDF', 'main');
 //  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
 cc.Class({
   "extends": cc.Component,
-  properties: {// foo: {
-    //     // ATTRIBUTES:
-    //     default: null,        // The default value will be used only when the component attaching
-    //                           // to a node for the first time
-    //     type: cc.SpriteFrame, // optional, default is typeof default
-    //     serializable: true,   // optional, default is true
-    // },
-    // bar: {
-    //     get () {
-    //         return this._bar;
-    //     },
-    //     set (value) {
-    //         this._bar = value;
-    //     }
-    // },
+  properties: {
+    oneSayLabel: {
+      "default": null,
+      type: cc.Label
+    }
   },
   // LIFE-CYCLE CALLBACKS:
-  // onLoad () {},
-  start: function start() {} // update (dt) {},
+  onLoad: function onLoad() {
+    this.oneSay();
+  },
+  start: function start() {
+    var that = this;
+    this.loadImg();
+    setInterval(function () {
+      that.oneSay();
+    }, 10000);
+  },
+  // update (dt) {},
+  loadImg: function loadImg() {
+    var that = this;
+    var container = cc.find('Canvas/mainBg/bingBg'); //图片呈现位置
 
+    var imgServeUrl = 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1';
+    var imgUrl = '';
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState == 4 && xhr.status >= 200 && xhr.status < 400) {
+        var response = JSON.parse(xhr.responseText);
+        imgUrl = 'https://cn.bing.com' + response.images[0].urlbase + '_720x1280.jpg';
+        cc.assetManager.loadRemote(imgUrl, function (err, texture) {
+          var sprite = new cc.SpriteFrame(texture);
+          container.spriteFrame = sprite; //创建一个使用图片资源的新节点对象
+
+          var starNode = new cc.Node(); //创建一个新节点
+
+          starNode.name = "star1";
+          starNode.setPosition(0, 0);
+          var starSprite = starNode.addComponent(cc.Sprite); //增加精灵组件
+
+          starSprite.spriteFrame = sprite; //设置精灵组件图片资源
+
+          starSprite.sizeMode = 'CUSTOM';
+          starSprite.node.width = cc.winSize.width;
+          starSprite.node.height = cc.winSize.height;
+          container.addChild(starNode); //场景中增加新节点
+        });
+      }
+    };
+
+    xhr.open("get", imgServeUrl, true);
+    xhr.send();
+  },
+  oneSay: function oneSay() {
+    var that = this;
+    var url = "https://v1.hitokoto.cn/";
+    var xhr = new XMLHttpRequest();
+    var oneSayLabel = cc.find("Canvas/mainBg/oneSay").getComponent(cc.Component);
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState == 4 && xhr.status >= 200 && xhr.status < 400) {
+        var response = JSON.parse(xhr.responseText);
+        if (that.oneSayLabel && that.oneSayLabel.string != null) that.oneSayLabel.string = response.hitokoto + ' -- ' + response.from;else if (oneSayLabel && oneSayLabel.string != null) oneSayLabel.string = response.hitokoto + ' -- ' + response.from;
+      }
+    };
+
+    xhr.open("get", url, true);
+    xhr.send();
+  }
 });
 
 cc._RF.pop();
