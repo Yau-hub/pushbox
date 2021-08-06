d95d53c048a671bc973859f2d564315cf5affec7
Yau <1173383184@qq.com>
2021-08-06 21:45:55 +0800

    test home

---
+++ build/pushboxbundle/cloudfunction/addClassicsLevel/index.js
@@ -0,0 +1,15 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+cloud.init()
+const db = cloud.database()
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  //插入数据
+  return await db.collection('classicsLevel').add ({
+    data: {
+      content: event.content, 
+      createTime: Date.now()
+    }
+  })  
+}
