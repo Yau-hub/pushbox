b5766ec4badb792dcac8e8e499e638adb563dacc
Yau-hub <1173383184@qq.com>
2021-08-06 11:27:07 +0800

    add bulid

---
+++ build/pushboxbundle/wechatgame/src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"wechatgame",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/main.fire",orientation:"",server:"",jsList:[],bundleVers:{internal:"70573",resources:"d3d6d",main:"6eaa6"}};
